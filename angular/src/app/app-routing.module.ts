7327ad664f271a2985c07a8561da8a91846ecce5
Ilya Kataev <kataev.i@gmail.com>
2019-05-17 12:28:04 +0300

    routes init

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'welcome' },
+  { path: 'welocme', component:  },
+  // Authorization
+  { path: 'login', component: NameComponent },
+  { path: 'register', component: NameComponent },
+  // Account
+  { path: 'credentional', component: NameComponent },
+  { path: 'favor', component: NameComponent },
+]; 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouteWelocmeComponent } from './route/wellcome/route-welocme/route-welocme.component';
+import { RouteWelcomeComponent } from './route/welcome/route-welcome/route-welcome.component';
+import { LoginComponent } from './route/auth/login/login.component';
+import { RegisterComponent } from './route/auth/register/register.component';
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RouteWelocmeComponent,
+    RouteWelcomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
