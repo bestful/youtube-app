ff167b5a5d75b24584261639e1781f78691d5fb1
Ilya Kataev <kataev.i@gmail.com>
2019-07-02 09:57:29 +0300

    merge

--- webapi/Startup.cs
+++ webapi/Startup.cs
@@ -41,7 +41,7 @@ namespace webapi
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
-                configuration.RootPath = "ClientApp/dist";
+                configuration.RootPath = "../angular/dist";
             });
             
             // Auto Documentation of api
@@ -107,7 +107,7 @@ namespace webapi
                 // To learn more about options for serving an Angular SPA from ASP.NET Core,
                 // see https://go.microsoft.com/fwlink/?linkid=864501
 
-                spa.Options.SourcePath = "ClientApp";
+                spa.Options.SourcePath = "../angular";
 
                 if (env.IsDevelopment())
                 {
--- webapi/install_packages.ps1
+++
@@ -1,9 +0,0 @@
-dotnet add package MySql.Data.EntityFrameworkCore 
-
-# Code generation
-dotnet add package Microsoft.AspNetCore.Razor.Design -v 2.1.9
-dotnet add package Microsoft.EntityFrameworkCore.Design -v 2.1.11
-dotnet add package Microsoft.VisualStudio.Web.CodeGeneration.Design -v 2.1.9
-
-# Docs for api
-dotnet add package Swashbuckle.AspNetCore -v 4.0.1
